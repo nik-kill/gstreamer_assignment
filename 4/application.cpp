0bce12a8f6ba15da48d1912f59c38306918dd6e2
Nikhil Gupta <nikhil062000@gmail.com>
2023-02-23 17:36:32 +0530

    assignment added

---
+++ 4/application.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b);
+
+int main(){
+    cout << "Sum of 5 & 6 is " << sum(5,6) << "\n";
+
+    return 0;
+}
---
+++ 4/shared_library.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){
+    return (a+b);
+}
